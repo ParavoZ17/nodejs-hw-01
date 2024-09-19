5e6ca5a58cf51fd41962dfcaa0bad1dab93f1f59
Mykola Naumenko <1712nn2012@gmail.com>
2024-09-19 12:14:56 +0200

    Initial commit

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,14 @@
-export const addOneContact = async () => {};
+import { readContacts } from "../utils/readContacts.js";
+import { createFakeContact } from "../utils/createFakeContact.js";
+import { writeContacts } from "../utils/writeContacts.js";
+
+
+export const addOneContact = async () => {
+    const contacts = await readContacts();
+    const newContact = createFakeContact();
+
+contacts.push(newContact);
+writeContacts(contacts);
+};
 
 addOneContact();
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,12 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  const newContacts = [];
+  for (let i = 0; i < number; i++) {
+    newContacts.push(createFakeContact());
+  }
+  await writeContacts(newContacts);
+};
 
 generateContacts(5);
