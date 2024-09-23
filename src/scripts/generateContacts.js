8462949d5f7a685dd6819703542239a0835f0ff1
Mykola Naumenko <1712nn2012@gmail.com>
2024-09-23 14:52:20 +0200

    newUpdate

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,12 +1,21 @@
 import { createFakeContact } from '../utils/createFakeContact.js';
 import { writeContacts } from '../utils/writeContacts.js';
+import { readContacts } from '../utils/readContacts.js';
 
 const generateContacts = async (number) => {
-  const newContacts = [];
-  for (let i = 0; i < number; i++) {
-    newContacts.push(createFakeContact());
+  try {
+    const excistingContacts = await readContacts();
+
+    const newContacts = [];
+    for (let i = 0; i < number; i++) {
+      newContacts.push(createFakeContact());
+    }
+
+    const updatedContacts = [...excistingContacts, ...newContacts];
+    await writeContacts(updatedContacts);
+  } catch (error) {
+    console.error(error);
   }
-  await writeContacts(newContacts);
 };
 
 generateContacts(5);
